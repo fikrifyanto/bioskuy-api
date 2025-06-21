731021dffe51e275c766f47a46655b5e2255fff9
fikrifyanto <fikrifyanto@gmail.com>
2025-06-21 22:28:29 +0700

    Set default timezone

--- src/main/java/com/bioskuy/api/ApiApplication.java
+++ src/main/java/com/bioskuy/api/ApiApplication.java
@@ -1,14 +1,22 @@
 package com.bioskuy.api;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
 public class ApiApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jakarta"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
