e337c7368a1a6f2d2477daf03367dbf4dd67ed75
fikrifyanto <fikrifyanto@gmail.com>
2025-06-21 22:21:32 +0700

    Remove unnecessary method di BaseEntity

--- src/main/java/com/bioskuy/api/entity/BaseEntity.java
+++ src/main/java/com/bioskuy/api/entity/BaseEntity.java
@@ -7,8 +7,6 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 
 @Getter
@@ -22,15 +20,4 @@ public abstract class BaseEntity {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
